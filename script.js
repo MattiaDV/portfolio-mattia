fb21b8c6032f1eabf4dd8b99cf3d4867c9370b4f
MattiaDV <mattiadv25105@gmail.com>
2025-02-18 18:50:50 +0100

    Prima parte

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let img = document.querySelector('.imagePersonal');
-    
-    if (img == true && img != undefined) {
-        alert(img);
-    } else {
-        alert("Mi dispiace ma l'elemento img non esiste: ". img);
-    }
-});
